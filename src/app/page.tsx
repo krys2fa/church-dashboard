048e57c3e1cf06f1fe4b31c6cb3b4888acf510d7
Christopher Amanor <christopher.k.amanor@gmail.com>
2024-12-28 15:03:51 +0000

    Fix hitch with homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,20 @@
-import Card from "@/components/Card";
+"use client";
 
-const data = [
-  { title: "Total Members", value: "256", color: "bg-blue-500" },
-  { title: "Events This Month", value: "12", color: "bg-green-500" },
-  { title: "Donations", value: "$12,500", color: "bg-yellow-500" },
-];
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-      {data.map((item) => (
-        <Card key={item.title} {...item} />
-      ))}
-    </div>
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      router.push("/login");
+    } else {
+      router.push("/dashboard");
+    }
+  }, [router]);
+
+  return <div className=''>Welcome to the Home Page</div>;
 }
